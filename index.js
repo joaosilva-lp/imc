2d17925bc7f3f46f8c73944d9b489747936efd2c
joao-silva-lp <joaosilva.lp@gmail.com>
2022-06-12 23:21:00 +0100

    addding BMI ranges

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const calcular = document.getElementById("calcular");
 const resultado = document.getElementById("resultado");
+let info = '';
 
 function imc(){
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
     const valorIMC = (peso / (altura * altura)).toFixed(2);
     console.log(`o imc é ${valorIMC}`);
-    resultado.textContent = valorIMC;
+    
+
+    if (valorIMC<18.5){
+        info = 'you are in the underweight range.'
+    } else if (valorIMC>=18.5 && valorIMC<25){
+        info = 'you are in the healthy weight range.'
+    } else if (valorIMC>=25 && valorIMC<30){
+        info = 'you are in the overweight range.'
+    } else if (valorIMC>=30 && imc<40){
+        info = 'you are in the obese range.'
+    }
+
+
+    resultado.textContent = `Your body mass index is ${valorIMC} and ${info}`;
 };
 
 calcular.addEventListener("click", imc);
