27898548c3ac2aada9786bbeaaa686a0adec123f
joao-silva-lp <joaosilva.lp@gmail.com>
2022-06-13 12:40:06 +0100

    added preventDefault() and some style

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const calcular = document.getElementById("calcular");
+const calcular = document.getElementById("calcular")
 const resultado = document.getElementById("resultado");
-let info = '';
 
 function imc(){
+    let info ='';
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
     const valorIMC = (peso / (altura * altura)).toFixed(2);
-    console.log(`o imc é ${valorIMC}`);
-    
+        
 
     if (valorIMC<18.5){
         info = 'you are in the underweight range.'
@@ -15,13 +14,18 @@ function imc(){
         info = 'you are in the healthy weight range.'
     } else if (valorIMC>=25 && valorIMC<30){
         info = 'you are in the overweight range.'
-    } else if (valorIMC>=30 && imc<40){
+    } else if (valorIMC>=30){
         info = 'you are in the obese range.'
+    } else{
+        info= 'the value is not valid, please try another'
     }
 
-
     resultado.textContent = `Your body mass index is ${valorIMC} and ${info}`;
+    console.log(resultado.textContent);
 };
 
-calcular.addEventListener("click", imc);
-
+calcular.addEventListener("click", function(event){
+    imc();
+    event.preventDefault();
+}
+);
