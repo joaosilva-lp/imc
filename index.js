3f04a2f5a73a3c14a107aef5fe749ae7198d17f0
João Silva <99473819+joaosilva-lp@users.noreply.github.com>
2022-11-15 15:30:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ function imc(){
         
 
     if (valorIMC<18.5){
-        info = 'you are in the underweight range.'
+        info = 'You are in the underweight range.'
     } else if (valorIMC>=18.5 && valorIMC<25){
-        info = 'you are in the healthy weight range.'
+        info = 'You are in the healthy weight range.'
     } else if (valorIMC>=25 && valorIMC<30){
-        info = 'you are in the overweight range.'
+        info = 'You are in the overweight range.'
     } else if (valorIMC>=30 && valorIMC<35){
-        info = 'you are in the obese range.'
+        info = 'You are in the obese range.'
     } else if (valorIMC>=35 && valorIMC<40){
-        info = 'you are in the excessive obese range.'
+        info = 'You are in the excessive obese range.'
     } else if (valorIMC>=40){
-        info = 'you are in the morbid obese range.'
+        info = 'You are in the morbid obese range.'
     } else{
-        info= 'the value is not valid, please try another'
+        info= 'The value is not valid, please try another'
     }
 
     resultado.textContent = `Your body mass index is ${valorIMC} and ${info}`;
